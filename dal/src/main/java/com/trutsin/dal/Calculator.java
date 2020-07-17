5c865627cc8f0afb78bfe3ba78fdefe75b6bcd1c
Sergey Trutsin <sergey.trutsin@gmail.com>
2020-07-17 12:01:15 +0300

    initial new repository

---
+++ dal/src/main/java/com/trutsin/dal/Calculator.java
@@ -0,0 +1,19 @@
+package com.trutsin.dal;
+
+public class Calculator {
+    public double add(final double firstValue, final double secondValue) {
+        return firstValue + secondValue;
+    }
+
+    public double subtract(final double firstValue, final double secondValue) {
+        return firstValue - secondValue;
+    }
+
+    public double multiply(final double firstValue, final double secondValue) {
+        return firstValue * secondValue;
+    }
+
+    public double divide(final double firstValue, final double secondValue) {
+        return firstValue / secondValue;
+    }
+}
---
+++ service/src/main/java/com/trutsin/service/GetCalculator.java
@@ -0,0 +1,11 @@
+package com.trutsin.service;
+
+import com.trutsin.dal.Calculator;
+
+public class GetCalculator {
+    Calculator calculator = new Calculator();
+
+    public static void main(String[] args) {
+
+    }
+}
